00afe38ebe1ffddb101959290dc6d980a7d24aea
Varun Gumma <t-gummavarun@microsoft.com>
2024-12-25 16:09:26 +0000

    fixed rms_norm

--- fairseq/modules/rms_norm.py
+++ fairseq/modules/rms_norm.py
@@ -1,8 +1,33 @@
 import torch
-from torch import nn
+import torch.nn as nn
+
+has_fused_rmsnorm = False
+has_pytorch_rmsnorm = False
+
+try:
+    from apex.normalization import FusedRMSNorm as _FusedRMSNorm
+
+    has_fused_rmsnorm = True
+
+    class FusedRMSNorm(_FusedRMSNorm):
+        @torch.jit.unused
+        def forward(self, x):
+            if not x.is_cuda:
+                return super().forward(x)
+            else:
+                with torch.cuda.device(x.device):
+                    return super().forward(x)
+
+except ImportError:
+    try:
+        from torch.nn import RMSNorm as PyTorchRMSNorm
+
+        has_pytorch_rmsnorm = True
+    except ImportError:
+        pass
 
 
-class RMSNorm(nn.Module):
+class NativeRMSNorm(nn.Module):
     """
     Implements Root Mean Square Normalization introduced in
     https://arxiv.org/abs/1910.07467.
@@ -16,10 +41,11 @@ class RMSNorm(nn.Module):
         eps (float): small value to avoid division by zero. Default: 1e-6
     """
 
-    def __init__(self, dim: int, eps: float = 1e-6) -> None:
+    def __init__(self, normalized_shape: int, eps: float = 1e-6) -> None:
         super().__init__()
         self.eps = eps
-        self.scale = nn.Parameter(torch.ones(dim))
+        self.normalized_shape = normalized_shape
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -34,7 +60,26 @@ class RMSNorm(nn.Module):
         x_normed = (
             x_fp32 * torch.rsqrt(x_fp32.pow(2).mean(-1, keepdim=True) + self.eps)
         ).type_as(x)
-        return x_normed * self.scale
+        return x_normed * self.weight
 
     def extra_repr(self) -> str:
-        return f"dim={self.scale.shape[0]}, eps={self.eps}"
+        return f"{self.normalized_shape}"
+
+
+def RMSNorm(normalized_shape, eps=1e-5, elementwise_affine=True, export=False):
+    if torch.jit.is_scripting() or torch.jit.is_tracing():
+        export = True
+    if not export and torch.cuda.is_available() and has_fused_rmsnorm:
+        return FusedRMSNorm(
+            normalized_shape=normalized_shape,
+            eps=eps,
+            elementwise_affine=elementwise_affine,
+        )
+    elif has_pytorch_rmsnorm:
+        return PyTorchRMSNorm(
+            normalized_shape=normalized_shape,
+            eps=eps,
+            elementwise_affine=elementwise_affine,
+        )
+    else:
+        return NativeRMSNorm(normalized_shape, eps=eps)
